e16dd8145639beaa558a54406412a02ed75e42f9
LeonMar0402 <125307192+LeonMar0402@users.noreply.github.com>
2024-06-24 14:05:43 +0200

    Test nicht unendelich

--- main.ts
+++ main.ts
@@ -1,24 +1,29 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    basic.showString("A")
     Würfel_oder_Münzwurf = 1
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
-    basic.showString("B")
     Würfel_oder_Münzwurf = 2
 })
 let Würfel_oder_Münzwurf = 0
+let einmal = 0
 let Kopf_oder_Zahl = 0
 Würfel_oder_Münzwurf = 0
 basic.showString("Würfel- oder Münzwurf? [a/b]")
 basic.forever(function () {
     if (Würfel_oder_Münzwurf == 1) {
-        basic.showString("Schütel um den Würfel zu werfen.")
+        if (einmal == 0) {
+            basic.showString("Schütel um den Würfel zu werfen.")
+            einmal = 1
+        }
         if (input.isGesture(Gesture.Shake)) {
             basic.showString("" + (randint(1, 6)))
         }
     }
     if (Würfel_oder_Münzwurf == 2) {
-        basic.showString("Schütel um die Münze zu werfen.")
+        if (einmal == 0) {
+            basic.showString("Schütel um die Münze zu werfen.", 200)
+            einmal = 1
+        }
         if (input.isGesture(Gesture.Shake)) {
             Kopf_oder_Zahl = randint(1, 2)
             if (Kopf_oder_Zahl == 1) {
